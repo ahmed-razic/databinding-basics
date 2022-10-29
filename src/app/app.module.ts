764ca53be0b6b02ae20da19152081250e0cef437
ahmed.razic <ahmed.razic@gmail.com>
2022-10-29 10:26:40 +0200

    assignment1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
+import { Assignment1Component } from './assignment1/assignment1.component';
 
 @NgModule({
-  declarations: [AppComponent, CarsComponent, CarComponent],
+  declarations: [AppComponent, CarsComponent, CarComponent, Assignment1Component],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
